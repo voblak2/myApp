69f6830f9b90232e4cc0d6bb60a16293975fdfd0
voblak2 <vinceoblak@vinces-mbp.lan>
2020-03-20 00:37:54 -0500

    hi

--- Program.cs
+++ Program.cs
@@ -6,39 +6,17 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            // First the variable rating is declared and set to 7.
-            var rating = 7;
+            var isHungry = true;
+var isRestaurantOpen = true;
 
-            // if the user rated the restaurant 5-stars, print "This food is the best! I could eat this every day."
-            if(rating == 5)
-            {
-                Console.WriteLine("This food is the best! I could eat this every day.");
-            }
-            // if the user rated the restaurant 4-stars, print "Good meal."
-            else if(rating == 4)
-            {
-                Console.WriteLine("Good meal.");
-            }
-            // if the user rated the restaurant 3-stars, print "It was OK."
-            else if(rating == 3)
-            {
-                Console.WriteLine("It was OK.");
-            }
-            // if the user rated the restaurant 2-stars, print "I did not like this, but it's cheap."
-            else if(rating == 2)
-            {
-                Console.WriteLine("I did not like this, but it's cheap.");
-            }
-            // if the user rated the restaurant 1-star,  print "I will never eat here again."
-            else if(rating == 1)
-            {
-                Console.WriteLine("I will never eat here again.");
-            }
-            // Validation for number entered outside of rating.
-            else
-            {
-                Console.WriteLine("Please enter a valid rating of 1-5.");
-            }
+if(isHungry && isRestaurantOpen)
+{
+    Console.WriteLine("Go to the restaurant!");
+}
+else
+{
+    Console.WriteLine("Don't go to the restaurant.");
+}
         }
     }
 }
